5a12d4a24569db43992c0d7cf1cd9628cade1179
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2022-11-06 19:50:22 +0100

    0.1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # notion_pandas
 A library that transforms notion tables into pandas dataframes by connecting with the notion API
+
+## Installation
+1. pip install -r ./requirements.txt
+2. Create an integration [here](https://www.notion.so/my-integrations "Title").
+3. Save your *token* from the previous page
+4. Save the id of your table. the address of your table will be similar to this  https://www.notion.so/1234v=567. You need to save the numbers after the / and before the v
+5. On the top left of your table visualization you have tre dots (...) click on that, after click on add connections and search for the integration you have create in point 2 and click confirm
+2. Use this library 😎
+
+## Example
+```py
+from notion_pandas import notion_pandas 
+import pandas as pd
+
+npd = notion_pandas("token", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+npd.to_csv('test.csv')
+
+df =  npd.return_pandas()
+df.head()
+```
---
+++ notion_pandas.py
@@ -0,0 +1,59 @@
+import os
+from notion_client import Client
+import pandas as pd 
+import json 
+
+#definition object notion_pandas
+class notion_pandas:
+    def __init__(self, authh, database_idd):  
+        try:
+            self.notion =  Client(auth=authh)
+            self.my_page = self.notion.databases.query(database_id=database_idd)
+            
+        except:
+            print('Please check your auth and database_id')
+
+    def change_user(self, auth):
+            try:
+                self.notion = Client(auth)
+            
+            except:
+                print('Please check your auth')
+    
+    def change_database(self, database_id):
+        try:
+            self.my_page = self.notion.databases.query(database_id)
+        except:
+            print('Please check your database_id')
+    
+    def return_pandas(self):
+        data = self.my_page['results']
+        #save my_page to json file
+        with open('./result.json', 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+        df = pd.read_json('./result.json')  
+        df.properties.to_json('./result.json', orient='records', force_ascii=False)
+        df = pd.read_json('./result.json')
+        col = df.columns  
+
+        for c in col:
+            type = dict(df[c][0])['type']
+            if type == 'checkbox' or type == 'number':
+                df[c] = df[c].apply(lambda x: dict(x)[type])
+            elif type == 'date':
+                df[c] = df[c].apply(lambda x: dict(dict(x)[type])['start'])
+            elif type == 'status':
+                df[c] = df[c].apply(lambda x: dict(dict(x)[type])['name'])
+            elif type == 'title':
+                df[c] = df[c].apply(lambda x: dict(list(dict(x)[type])[0])['text']['content'])
+        
+        #delete ./result.json
+        os.remove('./result.json')
+
+        return df
+    
+    def to_csv(self, path):
+        df = self.return_pandas()
+        df.to_csv(path, index=False)
+                
